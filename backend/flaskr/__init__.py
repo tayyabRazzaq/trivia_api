b346ce4fa5b1846422a21c5753e417c33df968d8
Kagaroat <tayyab.razzaq401@gmail.com>
2019-12-19 21:47:50 +0500

    docs: removed todo docs for add question

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -98,17 +98,6 @@ def create_app(test_config=None):
         except Exception:
             abort(STATUS_UNPROCESSABLE_ENTITY)
 
-    '''
-    @TODO:
-    Create an endpoint to POST a new question,
-    which will require the question and answer text,
-    category, and difficulty score.
-
-    TEST: When you submit a question on the "Add" tab,
-    the form will clear and the question will appear at the end of the last page
-    of the questions list in the "List" tab.
-    '''
-
     @app.route('/questions', methods=['POST'])
     def add_question():
         """
