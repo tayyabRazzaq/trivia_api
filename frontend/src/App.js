1424582bbf4cc62f466f8466a3437799269d6492
Kagaroat <tayyab.razzaq401@gmail.com>
2019-12-21 00:32:14 +0500

    refactor: index & app component refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,19 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
-
-// import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './stylesheets/App.css';
-import FormView from './components/FormView';
-import QuestionView from './components/QuestionView';
-import Header from './components/Header';
-import QuizView from './components/QuizView';
+import { FormView, QuestionView, Header, QuizView } from './components';
 
 
-class App extends Component {
-  render() {
-    return (
+export default () => (
     <div className="App">
-      <Header path />
-      <Router>
-        <Switch>
-          <Route path="/" exact component={QuestionView} />
-          <Route path="/add" component={FormView} />
-          <Route path="/play" component={QuizView} />
-          <Route component={QuestionView} />
-        </Switch>
-      </Router>
+        <Header path/>
+        <Router>
+            <Switch>
+                <Route path="/" exact component={QuestionView}/>
+                <Route path="/add" component={FormView}/>
+                <Route path="/play" component={QuizView}/>
+                <Route component={QuestionView}/>
+            </Switch>
+        </Router>
     </div>
-  );
-
-  }
-}
-
-export default App;
+);
---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as FormView } from './FormView';
+export { default as QuestionView } from './QuestionView';
+export { default as Header } from './Header';
+export { default as QuizView } from './QuizView';
