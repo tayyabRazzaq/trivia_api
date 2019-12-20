e7fd822208329ad1d26d250dcd11352fcc9efd8b
Kagaroat <tayyab.razzaq401@gmail.com>
2019-12-20 23:23:36 +0500

    test: added method now allowed fail case for add question test case

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -131,6 +131,18 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, STATUS_CREATED)
         self.assertEqual(json_data.get('success'), True)
 
+    def test_add_question_failed_method_not_allowed(self):
+        """
+        Fail case of add question test case with method not allowed error.
+
+        :return:
+        """
+        response = self.client.put('/questions', json={})
+        json_data = response.get_json()
+        self.assertEqual(response.status_code, STATUS_METHOD_NOT_ALLOWED)
+        self.assertEqual(json_data.get('success'), False)
+        self.assertEqual(json_data.get('message'), ERROR_MESSAGES[STATUS_METHOD_NOT_ALLOWED])
+
     def tearDown(self):
         """
         Execute after reach test.
